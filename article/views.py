f3113c722d0f3621b07bad922ab90a653b7b8215
cyunzing <cyunzing@gmail.com>
2016-10-23 21:11:10 +0800

    improve

--- article/views.py
+++ article/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
+from django.http import Http404
 
 
 # Create your views here.
@@ -11,12 +12,37 @@ def home(request):
 	#return HttpResponse('Hello World, Django')
 
 
-def detail(request, my_args):
-	post = Article.objects.all()[int(my_args)]
-	_str = ('title = %s,\ncategory = %s,\ndate_time = %s,\ncontent = %s' % (post.title, post.category, post.date_time, post.content))
-	return HttpResponse(_str)
+def detail(request, id):
+	try:
+		post = Article.objects.get(id = str(id))
+#		post = Article.objects.all()[int(my_args)]
+	except Article.DoesNotExist:
+		raise Http404
+	return render(request, 'post.html', {'post': post})
+#	_str = ('title = %s,\ncategory = %s,\ndate_time = %s,\ncontent = %s' % (post.title, post.category, post.date_time, post.content))
+#	return HttpResponse(_str)
 
 
 def test(request):
 	return render(request, 'test.html', {'current_time': datetime.now()})
 
+
+def archives(request):
+	try:
+		post_list = Article.objects.all()
+	except Article.DoesNotExist:
+		raise Http404
+	return render(request, 'archives.html', {'post_list': post_list, 'error': False})
+
+
+def about_me(request):
+	return render(request, 'aboutme.html')
+
+
+def search_tag(request):
+	try:
+		post_list = Article.objects.filter(category_iexact = tag)
+	except Article.DoesNotExist:
+		raise Http404
+	return render(request, 'tag.html', {'post_list': post_list})
+	
